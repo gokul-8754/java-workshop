a75dfde1e2d52b194a4f9f80c0bfd842765f13ee
gokul8754 <gokulpandyd24it@vetias.ac.in>
2025-07-07 14:43:23 +0530

    committoday

---
+++ src/com/vetias/java/Stringbuilderexample.java
@@ -0,0 +1,21 @@
+package com.vetias.java;
+
+public class Stringbuilderexample {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("java");
+        sb.append(" is");
+        sb.append(" the");
+        sb.append(" most");
+        sb.append(" used");
+        sb.append(" language");
+        sb.append(" for");
+        sb.append(" enetrprise");
+        sb.append(" applications");
+        System.out.println(sb.toString());
+        // sb.append("java").append("is").append("prog");
+        // System.out.println(sb.toString());
+
+
+    }
+    }
