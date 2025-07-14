8097a37996a9be3c261c358804ab8d4acb8d0237
gokul8754 <gokulpandyd24it@vetias.ac.in>
2025-07-14 10:54:27 +0530

    data application

---
+++ src/main/java/com/vetias/java/beans/Organization.java
@@ -0,0 +1,12 @@
+package com.vetias.java.beans;
+
+public record Organization(String name,
+String completeAddress,
+String website,
+String email,
+String phoneNumber,
+long registrationData) {
+ }
+
+
+
---
+++ src/main/java/com/vetias/java/beans/TempdataApplication.java
@@ -0,0 +1,11 @@
+package com.vetias.java.beans;
+
+public class TempdataApplication {
+    public static void main(String[] args) {
+        Organization organization=new Organization("Vetias","Thindal","www.vetias.com","vetias.ac.in",
+        "1234567890",2023);
+        System.out.println(organization);
+
+    }
+
+}
